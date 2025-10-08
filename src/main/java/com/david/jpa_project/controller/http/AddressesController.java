d1e99ee242eed628389cca1b875c7f9442abadf6
David Rodriguez <devdevbadillo@gmail.com>
2025-10-07 21:52:14 -0600

    feat: Agregando las consultas de Addreses con Ordenamiento e inicio de manejo de excepciones

--- src/main/java/com/david/jpa_project/controller/http/AddressesController.java
+++ src/main/java/com/david/jpa_project/controller/http/AddressesController.java
@@ -3,13 +3,16 @@ package com.david.jpa_project.controller.http;
 import com.david.jpa_project.controller.dto.out.AddressOut;
 import com.david.jpa_project.controller.dto.out.PageOut;
 import com.david.jpa_project.services.interfaces.IAddressesService;
-import org.springframework.data.domain.Page;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@Validated
 @RestController
-@RequestMapping("/v1/addresses")
+@RequestMapping(value = "/v1/addresses", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AddressesController {
 
     private final IAddressesService addressesService;
@@ -18,12 +21,22 @@ public class AddressesController {
         this.addressesService = addressesService;
     }
 
-    @GetMapping("/{city}")
-    public PageOut<AddressOut> getAddressesByCity(
+    @GetMapping("/city/{city}")
+    public ResponseEntity<PageOut<AddressOut>> getAddressesByCity(
             @PathVariable("city") String city,
             @RequestParam("page") int page,
             @RequestParam("size") int size
     ) {
-        return addressesService.getAddressesByCity(city, page, size);
+        return ResponseEntity.ok(addressesService.getAddressesByCity(city, page, size));
+    }
+
+    @GetMapping("/country/{pattern}")
+    public ResponseEntity<PageOut<AddressOut>> getAddressesByCountry(
+            @PathVariable("pattern") String pattern,
+            @RequestParam("page") @Min(value = 0, message = "El valor de la pagina tiene que ser mayor o igual a 0") int page,
+            @RequestParam("size") @Min(value = 1, message = "El valor del tamaño tiene que ser mayor o igual a 1") int size,
+            @RequestParam("orderByCity") @Pattern(regexp = "asc|desc", message = "El valor de orderByCity tiene que ser asc o desc") String orderByCity
+    ) {
+        return ResponseEntity.ok(addressesService.getAddressesByCountryLike(pattern, page, size, orderByCity));
     }
 }
--- src/main/java/com/david/jpa_project/model/repositories/jpql/AddressRepository.java
+++ src/main/java/com/david/jpa_project/model/repositories/jpql/AddressRepository.java
@@ -7,14 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     @Query("SELECT a FROM Address a WHERE a.city = :city")
     Page<Address> findAddressesByCity(String city, Pageable pageable);
 
-    @Query("SELECT a FROM Address a WHERE a.country = :country")
-    List<Address> findAddressesByCountry(String country);
+    @Query("SELECT a FROM Address a WHERE a.country LIKE %:country%")
+    Page<Address> findAddressesByCountryLike(String country, Pageable pageable);
 }
