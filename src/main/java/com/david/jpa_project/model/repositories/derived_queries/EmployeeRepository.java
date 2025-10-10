44513e7bac29d7f5ce68dd544bc1d763341eacb5
David Rodriguez <devdevbadillo@gmail.com>
2025-10-09 23:31:16 -0600

    feat: Agregando las consultas de Ordenes con projecciones y consultas nativas

--- src/main/java/com/david/jpa_project/model/repositories/derived_queries/EmployeeRepository.java
+++ src/main/java/com/david/jpa_project/model/repositories/derived_queries/EmployeeRepository.java
@@ -1,20 +1,9 @@
 package com.david.jpa_project.model.repositories.derived_queries;
 
 import com.david.jpa_project.model.entities.entity.Employee;
-import com.david.jpa_project.model.entities.enums.JobTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findEmployeeById(Long id);
-
-    void deleteEmployeeById(Long id);
-
-    List<Employee> findEmployeesByJobTitle(JobTitle jobTitle);
-
-    List<Employee> findEmployeesByJobTitleIn(List<JobTitle> jobTitles);
 }
--- src/main/java/com/david/jpa_project/services/interfaces/IOrdersService.java
+++ src/main/java/com/david/jpa_project/services/interfaces/IOrdersService.java
@@ -1,4 +1,9 @@
 package com.david.jpa_project.services.interfaces;
 
+import com.david.jpa_project.controller.dto.out.OrderInfoOut;
+
 public interface IOrdersService {
+
+    OrderInfoOut findOrderByIdWithInformation(Long orderId);
+
 }
