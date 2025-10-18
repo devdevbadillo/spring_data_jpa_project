001a129eceb7caaafa65828e94e7eff1e783c213
David Rodriguez <devdevbadillo@gmail.com>
2025-10-18 11:23:36 -0600

    feat: Terminando las pruebas unitarias del servicio de Orders

---
+++ src/main/java/com/david/jpa_project/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.david.jpa_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
